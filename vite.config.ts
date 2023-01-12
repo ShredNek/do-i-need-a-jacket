94b315f2609a9e087897a23dcea95e26045c3583
Daniel Lee <danielleemusic98@gmail.com>
2023-01-12 23:52:12 +1100

    final changes for tonight

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,5 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
-export default ({ mode }) => {
-  // Load app-level env vars to node-level env vars.
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    // To access env vars here use process.env.TEST_VAR
-    envDir: process.env.TEST_VAR,
-  });
+export default () => {
+  return defineConfig({});
 };
